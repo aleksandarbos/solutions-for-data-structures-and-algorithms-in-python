5005b45cc5d2a63e4b4ed92d181c66f4b744f4f4
Aleksandar <aleksandarbos.ns@gmail.com>
2022-11-10 13:07:27 +0100

    feat:further improved merging, added find kth el

--- custom-10.1.py
+++ custom-10.1.py
@@ -9,40 +9,47 @@ def merge(A, B):
     if A == [] or B == []:
         raise Exception('Both A and B need to have at least one element.')
 
-    result = []
-
     while count_a < len(A) and count_b < len(B):
         if A[count_a] < B[count_b]:
-            result.append(A[count_a])
+            yield A[count_a]
             count_a += 1
         elif A[count_a] > B[count_b]:
-            result.append(B[count_b])
+            yield B[count_b]
             count_b += 1
 
     if count_a >= len(A):
         for i in range(count_b, len(B)):
-            result.append(B[i])
+            yield B[i]
     elif count_b >= len(B):
         for i in range(count_a, len(A)):
-            result.append(A[i])
+            yield A[i]
 
+def find_kth_element(A, B, k):
+    """
+    finds k-th element of the two sorted arrays A and B.
+    time complexity: O(k)
+    """
+    result = None
+    for i, kth in enumerate(merge(A, B)):
+        if i == k-1:
+            result = kth
     return result
 
 if __name__ == "__main__":
     A = [3, 5, 7, 9]
     B = [1, 4, 8, 11, 13, 16]
 
-    C = merge(A, B)
+    C = list(merge(A, B))
     assert C == [1, 3, 4, 5, 7, 8, 9, 11, 13, 16]
 
     A = [0]
     B = [2]
-    C = merge(A, B)
+    C = list(merge(A, B))
     assert C == [0, 2]
 
     A = [4]
     B = [1, 5, 6]
-    C = merge(A, B)
+    C = list(merge(A, B))
     assert C == [1, 4, 5, 6]
 
     try:
@@ -52,5 +59,11 @@ if __name__ == "__main__":
 
     A = [3, 6, 9]
     B = [8]
-    C = merge(A, B)
+    C = list(merge(A, B))
     assert C == [3, 6, 8, 9]
+
+    A = [1, 3, 11]
+    B = [4, 8] # C = [1, 3, 4, 8, 11]
+    assert find_kth_element(A, B, k=3) == 4
+    assert find_kth_element(A, B, k=1) == 1
+    assert find_kth_element(A, B, k=5) == 11
