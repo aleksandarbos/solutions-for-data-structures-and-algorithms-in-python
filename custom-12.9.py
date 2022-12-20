cc93f258f0d6d9fd8b60b2f03dc227ec50ccd5c0
Aleksandar <aleksandarbos.ns@gmail.com>
2022-12-20 12:44:19 +0100

    feat:count sort

---
+++ custom-12.9.py
@@ -0,0 +1,37 @@
+"""
+count sort and radix sort implementation
+"""
+
+def count_sort(array):
+    size = len(array)
+    output = [0] * size
+
+    # Initialize count array
+    count = [0] * 10
+
+    # Store the count of each elements in count array
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    # Store the cummulative count
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    # Find the index of each element of the original array in count array
+    # place the elements in output array
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = array[i]
+        count[array[i]] -= 1
+        i -= 1
+
+    # Copy the sorted elements into original array
+    for i in range(0, size):
+        array[i] = output[i]
+
+
+if __name__ == "__main__":
+    S = [3, 5, 1, 2, 4]
+    count_sort(S)
+
+    assert S == [1, 2, 3, 4, 5]
