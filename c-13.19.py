e925499543864041e3668c1ee76df89569ceea7d
Aleksandar <aleksandarbos.ns@gmail.com>
2023-01-17 16:39:10 +0100

    feat:brute count

---
+++ c-13.19.py
@@ -0,0 +1,34 @@
+"""
+The count method of Python's str class reports the maximum number of non-overlapping occurrences of
+a pattern within a string. For example, the call 'abababa'.count('aba') results with 2 (not 3). Adapt
+the brute-force pattern-matching algorithm to implement a function, count_brute(T, P) with similar outcome.
+"""
+
+def find_brute(T, P, start=0):
+    n, m = len(T), len(P)
+    for i in range(start, n-m+1):
+        k = 0
+        while k < m and T[i + k] == P[k]:
+            k += 1
+        if k == m:
+            return i
+    return -1
+
+def count_brute(T, P):
+    m = len(P)
+    k = 0
+    cnt = 0
+    while True:
+        j = find_brute(T, P, start=k)
+        if j == -1:
+            break
+        else:
+            cnt += 1
+            k = j + m
+    return cnt
+
+if __name__ == "__main__":
+    T = 'alaabababasada'
+    P = 'aba'
+
+    assert count_brute(T, P) == T.count(P)
