52ce69ff05619ff4f604d43c9ea866307e46eae0
Aleksandar <aleksandarbos.ns@gmail.com>
2023-02-13 12:33:52 +0100

    feat:added c-14.37 impl remove_vertex

--- shared_14_chapter.py
+++ shared_14_chapter.py
@@ -11,6 +11,11 @@ class Vertex(object):
     def element(self):
         return self._element
 
+    def __repr__(self):
+        if hasattr(self, '_element') is not None:
+            return f'Vertex[{self._element}]'
+        else:
+            return super().__repr__()
 
 class Edge(object):
     __slots__ = '_origin', '_destination', '_element'
@@ -97,6 +102,26 @@ class Graph(object):
         adj = self._outgoing if outgoing else self._incoming
         return len(adj[v])
 
+    def remove_vertex(self, v):
+        """
+        removes vertex `v` and it's related edges in O(deg(v)) amortized time.
+        exercise: c-14.37.
+        """
+        outgoing_edges = list(self.incident_edges(v))
+        incoming_edges = []
+        if self.is_directed():
+            incoming_edges = list(self.incident_edges(v, outgoing=False))
+            if v in self._incoming:
+                del self._incoming[v]
+            for e in incoming_edges: # O(deg(v))
+                u = e.opposite(v)
+                del self._outgoing[u][v]
+        if v in self._outgoing:
+            del self._outgoing[v]
+        for e in outgoing_edges: # O(deg(v))
+            u = e.opposite(v)
+            del self._outgoing[u][v]
+
 def prim_jarnik(g):
     from heapq import heappush, heappop
 
@@ -134,9 +159,9 @@ def prim_jarnik(g):
 if __name__ == "__main__":
     # make a triangle shaped undirected cyclic graph
     g = Graph()
-    v1 = g.insert_vertex()
-    v2 = g.insert_vertex()
-    v3 = g.insert_vertex()
+    v1 = g.insert_vertex(element='v1')
+    v2 = g.insert_vertex(element='v2')
+    v3 = g.insert_vertex(element='v3')
 
     e1 = g.insert_edge(v1, v2, element=10)
     e2 = g.insert_edge(v2, v3, element=20)
@@ -151,3 +176,26 @@ if __name__ == "__main__":
     assert g.degree(v1) == 2
     assert g.get_edge(v3, v1) == e3
     assert prim_jarnik(g) == [e2, e1]
+
+    g.remove_vertex(v1)
+    assert g.edges() == {e2}
+    assert g.vertices() == {v2, v3}
+    assert e2.endpoints() == (v2, v3)
+
+    # directed triangle graph v1 -> v2, v1 -> v3, v2 -> v3
+    g_dir = Graph(directed=True)
+    v1 = g_dir.insert_vertex(element='v1')
+    v2 = g_dir.insert_vertex(element='v2')
+    v3 = g_dir.insert_vertex(element='v3')
+
+    e1 = g_dir.insert_edge(v1, v2, element=10)
+    e2 = g_dir.insert_edge(v2, v3, element=20)
+    e3 = g_dir.insert_edge(v1, v3, element=30)
+
+    assert g_dir.edge_count() == 3
+    assert g_dir.vertex_count() == 3
+    assert g_dir.is_directed()
+
+    g_dir.remove_vertex(v3)
+    assert g_dir.edges() == {e1}
+    assert g_dir.vertices() == {v1, v2}
