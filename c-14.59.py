5ad05e2a1910f635d9806fbfed21629661932823
Aleksandar <aleksandarbos.ns@gmail.com>
2023-02-25 11:05:48 +0100

    docs:added note about dijkstra's algo for negative wgts

--- c-14.59.py
+++ c-14.59.py
@@ -3,6 +3,9 @@ C-14.59 Design an efficient algorithm for finding a longest directed path from a
 vertex s to a vertex t of an acyclic weighted directed graph G . Specify the
 graph representation used and any auxiliary data structures used. Also,
 analyze the time complexity of your algorithm.
+
+NOTE: dijkstra algorithm is failing with negative edges for certain edge cases
+such as displayed in here: https://stackoverflow.com/a/8081947/5736491
 """
 
 from heapq import heapify, heappop, heappush
